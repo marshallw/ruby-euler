e20b872276f2d8b346ce34c26f540f848f68c9bf
Marshall Wilson <marshallw@iqmetrix.com>
2016-05-16 13:30:56 -0600

    change
    change

--- euler1.rb
+++ euler1.rb
@@ -3,8 +3,7 @@ sum = 0
 for num in 1..upper do
     if ((num % 3 == 0) || (num % 5 == 0)) then
        sum += num
-        puts "#{num} is a multiple of 3 and 5!"
     end
 end
 
-puts "the final sum is #{sum}"
+puts sum
